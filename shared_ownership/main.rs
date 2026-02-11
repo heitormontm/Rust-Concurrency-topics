c3b7e6971c2bcff8cd4eec4e750e941b7db98d8a
heitormontm <heitormacedo1234@gmail.com>
2026-02-11 18:19:30 -0300

    Updating: Shared Ownership

--- shared_ownership/main.rs
+++ shared_ownership/main.rs
@@ -3,9 +3,11 @@
 
 static NUMBERS: [i32; 3] = [1, 2, 3]
 
-// By convention, we use SCREAMING_SNAKE_CASE to declare them 
-// As the thread can't live more than the main function, 'thread::spawn' doesn't run, requiring
-// closure to be ''static'
+/*
+    By convention, we use SCREAMING_SNAKE_CASE to declare them 
+As the thread can't live more than the main function, 'thread::spawn' doesn't run, requiring
+closure to be ''static' 
+                                                                                            */
 let mut numbers: Box<[usize]> = Vec::from_iter(0..=1000).into_boxed_slice();
 let numbers: &mut [usize] = &mut numbers[..];
 
@@ -17,7 +19,7 @@ let t = thread::spawn(|| { // Error on numbers lifetime
 
 let average = t.join().unwrap();
 
-// In order to make our data static, we can leak the memory
+/* To make the data 'static, we can intentionally leak the memory. */
 let numbers: Box<[usize]> = Vec::from_iter(0..=1000).into_boxed_slice();
 let numbers: &'static mut [usize] = Box::leak(numbers);
 
@@ -31,6 +33,7 @@ let average = t.join().unwrap();
 
 // Reference Counting
 // Allows us to have multiple owners of the same reference
+
 std::rc::Rc //for single-threaded use cases -> Rc = Reference Counted
 
 std::sync::Arc // for multi-threaded  -> Arc = Atomic Reference Counted
@@ -56,16 +59,19 @@ thread::spawn({ // Scope starts
 
 dbg!(a); // Original 'a'
 
-// Unsafe an UB (Undefined Behavior)
+// Unsafe and UB (Undefined Behavior)
 let a = [10, 20, 30];
 let index = 10; // Wrong 
 
 let x = unsafe {a.get_unchecked(index)};
 println!("{x}"); // It may print garbage, crash or appear to work
 
-// Interior Mutability 
-// Mutates data through an immutable reference by checking borrowing rules at runtime, using types like `Cell`, `RefCell`, etc.
-// Mutability Container types:
+/*  Interior Mutability 
+Mutates data through an immutable reference by checking borrowing rules at 
+runtime, using types like `Cell`, `RefCell`, etc.
+
+Mutability Container types:
+*/
 
 /* Cell<T> (simple interior mutability)
 - .get() and .set() methods for modifying
@@ -196,7 +202,7 @@ fn main() {
 /*We can opt out of the traits by adding a PhantomData type 
 containing something which is, for example, not Sync.
                                                             */
-use std::marker::PhantomData;
+use std::marker::PhantomData; // PhantomData has zero runtime size, it exists only for the type system
 
 struct X {
     handle: i32,
